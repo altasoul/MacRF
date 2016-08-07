e81013868c12e611080caa75d2afbecdc9965e5f
Tom Soulanille <soul@prama.com>
2016-08-06 21:37:34 -0400

    MacRF.py: get name, attrs, data for each resource

--- MacRF.py
+++ MacRF.py
@@ -14,63 +14,53 @@ class ResourceFork:
         self.filename = filename
         name = os.path.join(filename, '..namedfork/rsrc')
         f = self.f = open(name, 'rb') # OTTA: writing to resource forks        
-        mm = self.mm = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
+        mm = self.mappedRF = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
         self.o_data, self.o_map, self.l_data, self.l_map = unpack('>IIII', mm[:16])
         # OTTA: consistency checks
         self.app_data = memoryview(mm[128:256])
         
-        d = self.resource_data = memoryview(mm[self.o_data:self.o_data+self.l_data])
-        m = self.resource_map = memoryview(mm[self.o_map:self.o_map+self.l_map])
+        rdata = self.resource_data = memoryview(mm[self.o_data:self.o_data+self.l_data])
+        rmap = self.resource_map = memoryview(mm[self.o_map:self.o_map+self.l_map])
 
-        self.file_reference_number = unpack('>H', m[20:22])[0]
-        self.resource_file_attributes = unpack('2s', m[22:24])
-        o_tl = self.o_type_list = unpack('>H', m[24:26])[0]
-        o_nl = self.o_name_list = unpack('>H', m[26:28])[0]
+        # Parse the resource map
+        self.file_reference_number = unpack('>H', rmap[20:22])[0]
+        self.resource_file_attributes = unpack('2s', rmap[22:24])[0]
+        o_tl = self.o_type_list = unpack('>H', rmap[24:26])[0]
+        o_nl = self.o_name_list = unpack('>H', rmap[26:28])[0]
 
-        n = self.n_types = unpack('>H', m[o_tl:o_tl+2])[0]+1
-        tv = memoryview(self.resource_map[o_tl+2:o_tl+2+8*n])
-        Type = namedtuple('Type', ['name', 'n', 'offset'])
-        #Wish this worked:
-        #tv.cast('B', shape=[n, 8])
-        #types = [Type(unpack('>4sHH', tv[i])) for i in range(n)]
+        n = self.n_types = unpack('>H', rmap[o_tl:o_tl+2])[0] + 1
+        #type_list_v = memoryview(self.resource_map[o_tl:o_tl+2+8*n])
+        type_list_v = memoryview(self.resource_map[o_tl:o_nl])
+        name_list_v = memoryview(self.resource_map[o_nl:])
+        Type = namedtuple('Type', ['code', 'n', 'id', 'attrs', 'name', 'data'])
         types = []
         for i in range(n):
-            code, p, offset = unpack('>4sHH', tv[8*i:8*(i+1)])
-            types.append(Type(code, p+1, offset))
-
-
-
-
-        import pdb
-        pdb.set_trace()
-
-
-
-        f.seek(self.o_map)
-        m = self.resource_map = f.read(self.l_map)
-        self.file_reference_number = unpack('>H', m[20:22])[0]
-        self.resource_file_attributes = m[22:24]
-        o_tl = self.o_type_list = unpack('>H', m[24:26])[0]
-        o_nl = self.o_name_list = unpack('>H', m[26:28])[0]
-        n = self.n_types = unpack('>H', m[28:30])[0]+1
-
-
-
-        tv = memoryview(self.resource_map[30:30+8*n])
-        tv.cast('B', shape=[n, 8])
-        Type = namedtuple('Type', ['name', 'n', 'offset'])
-        types = [Type(unpack('>4sHH', tv[i])) for i in range(n)]
+            code, p, offset = unpack('>4sHH', type_list_v[2+8*i:2+8*(i+1)])
+            rid, o_name, attrs, tmsb, t = unpack('>HHBBH', type_list_v[offset:offset+8])
+            o_rdat = tmsb<<16 + t
+            l_rdat = unpack('>L', rdata[o_rdat:o_rdat+4])[0]
+            rdat = rdata[o_rdat+4:o_rdat+4+l_rdat]
+            if o_name == 0xffff:
+                name = b''
+            else:
+                name_len = name_list_v[o_name]
+                name = name_list_v[o_name+1:o_name+1+name_len].tobytes()
+            types.append(Type(code, p + 1, rid, attrs, name, rdat))
+        self.types = types
         return self
 
     def close(self):
+        self.mappedRF.close()
         self.f.close()
 
 
 def main():
     import pdb
+    from pprint import pprint
     name = '/Users/soul/Projects/Nova-decode/various/One outfit'
     rf = ResourceFork()
     f = rf.open(name)
+    pprint(f.__dict__)
     pdb.set_trace()
     f.close()
     
